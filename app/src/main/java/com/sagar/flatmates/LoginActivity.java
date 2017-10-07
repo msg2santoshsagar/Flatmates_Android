0d81d99d51e3ccf0ac4216b59db687a2e538f8c5
santosh sagar <msg2santoshsagar@gmail.com>
2017-10-08 02:06:07 +0530

    Dashboard activity added

--- app/src/main/java/com/sagar/flatmates/LoginActivity.java
+++ app/src/main/java/com/sagar/flatmates/LoginActivity.java
@@ -1,13 +1,36 @@
 package com.sagar.flatmates;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static  final String          TAG         = LoginActivity.class.getSimpleName();
+    private         final LoginActivity   CONTEXT     = LoginActivity.this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Login Button clicked");
+                Intent intent = new Intent( CONTEXT , Dashboard.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
